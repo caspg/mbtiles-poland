9e6263b8e9a9f63caa35261c6e99985bb1bc470f
Kacper Golinski <kacper.golinski@gmail.com>
2022-10-10 18:43:53 +0200

    add linux go_server binary

---
+++ build_linux_go_server.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+cd ./go_server
+
+CC=x86_64-linux-musl-gcc CXX=x86_64-linux-musl-g++ GOARCH=amd64 GOOS=linux CGO_ENABLED=1 go build -ldflags "-linkmode external -extldflags -static" -o go_server_linux_x86_64
