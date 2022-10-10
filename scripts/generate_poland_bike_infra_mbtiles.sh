5d95cd57cc2d64896cd9d67c1db4faa947902f54
Kacper Golinski <kacper.golinski@gmail.com>
2022-10-10 18:34:02 +0200

    updates

--- scripts/generate_poland_bike_infra_mbtiles.sh
+++ scripts/generate_poland_bike_infra_mbtiles.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-rm -rf tmp
-mkdir tmp
+rm -rf ../tmp
+mkdir ../tmp
 
 # https://download.geofabrik.de/europe/poland.html
 
-curl https://download.geofabrik.de/europe/poland-latest.osm.pbf -o tmp/input.osm.pbf
-# curl https://download.geofabrik.de/europe/poland/pomorskie-latest.osm.pbf -o tmp/input.osm.pbf
-# curl https://download.geofabrik.de/europe/poland/dolnoslaskie-latest.osm.pbf -o tmp/input.osm.pbf
+# curl https://download.geofabrik.de/europe/poland-latest.osm.pbf -o ../tmp/input.osm.pbf
+curl https://download.geofabrik.de/europe/poland/pomorskie-latest.osm.pbf -o ../tmp/input.osm.pbf
+# curl https://download.geofabrik.de/europe/poland/dolnoslaskie-latest.osm.pbf -o ../tmp/input.osm.pbf
 
-osmium tags-filter tmp/input.osm.pbf \
+osmium tags-filter ../tmp/input.osm.pbf \
   w/highway=cycleway \
   w/bicycle=designated \
   w/cycleway=lane \
@@ -24,18 +24,18 @@ osmium tags-filter tmp/input.osm.pbf \
   w/bicycle=yes \
   w/highway=footway \
   w/oneway:bicycle=no \
-  -o tmp/out.osm.pbf
+  -o ../tmp/out.osm.pbf
 
-osmconvert tmp/out.osm.pbf -o=tmp/out.o5m
+osmconvert ../tmp/out.osm.pbf -o=../tmp/out.o5m
 
-osmfilter tmp/out.o5m \
+osmfilter ../tmp/out.o5m \
   --ignore-dependencies \
   --drop-ways="mtb:scale= or route=mtb or informal=yes or highway=proposed or highway=construction" \
   --keep-ways="highway=cycleway or ( oneway:bicycle=no and oneway=yes ) or ( oneway:bicycle=no and oneway=cycleway ) or ( bicycle=yes and highway=footway ) or ( bicycle=permissive and highway=footway ) or ( foot=designated and bicycle=designated and segregated=yes ) or ( foot=designated and bicycle=designated and segregated=no ) or cycleway=track or cycleway:*=track or cycleway:*=lane or cycleway=lane or cycleway:*=opposite_lane or cycleway=opposite_lane" \
-  >tmp/out.osm
+  >../tmp/out.osm
 
-OSM_CONFIG_FILE=./config/osmconf_bike_infra.ini ogr2ogr -f GeoJSON tmp/out.geojson tmp/out.osm lines
+OSM_CONFIG_FILE=./config/osmconf_bike_infra.ini ogr2ogr -f GeoJSON ../tmp/out.geojson ../tmp/out.osm lines
 
 # `-z14`: Only generate zoom levels 0 through 14
 # `-l`: layer name
-tippecanoe -z14 -o data/bike_infra.mbtiles -l default --drop-fraction-as-needed tmp/out.geojson
+tippecanoe --force -z14 -o ../data/bike_infra.mbtiles -l default --drop-fraction-as-needed ../tmp/out.geojson
