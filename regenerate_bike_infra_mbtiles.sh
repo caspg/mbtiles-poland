008dd32e4672d134a2c9dbde149e2ee9cc32b6db
Kacper Golinski <kacper.golinski@gmail.com>
2022-10-13 16:58:46 +0200

    add debug logs and remove data/ and tmp/

--- regenerate_bike_infra_mbtiles.sh
+++ regenerate_bike_infra_mbtiles.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# just for debugging
+curl https://download.geofabrik.de/europe/poland.html
+
 # https://stackoverflow.com/a/24112741/4490927
 parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
 cd "$parent_path"
@@ -7,9 +10,11 @@ cd "$parent_path"
 cd mbtiles_scripts
 bash generate_poland_bike_infra_mbtiles.sh
 
+echo "replacing old mbtiles with new"
 cd ..
 rm -f data/bike_infra.mbtiles
 mv data/bike_infra.tmp.mbtiles data/bike_infra.mbtiles
 
+echo "restarting mbtiles_poland_go_server service"
 # full path to `service` is required. To find path run `which service`.
 /usr/sbin/service mbtiles_poland_go_server restart
